28c710032fed7af72fa4b226258ee58967bb3f76
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2024-11-15 13:39:53 -0300

    tela de home sendo construida

--- PORTIFOLIO/proj1-ANGULAR-FIREBASE/crud-usuarios/src/app/pages/login/login.component.ts
+++ PORTIFOLIO/proj1-ANGULAR-FIREBASE/crud-usuarios/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  userName: String;
+
+  constructor(private rota: Router) {
+
+  }
+
+  login(){
+    sessionStorage.setItem("user", "this.userName");
+
+    this.rota.navigate(["home"]);
+  }
 
 }
